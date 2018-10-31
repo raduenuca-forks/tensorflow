0890ad313e4e36a8b9b7858d27e0f659d5c8b0e1
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2018-10-31 13:58:44 +0100

    Update configure flags

--- tc-vars.sh
+++ tc-vars.sh
@@ -56,14 +56,11 @@ elif [ "${OS}" = "Darwin" ]; then
     TF_NEED_JEMALLOC=0
 fi;
 export TF_NEED_JEMALLOC
-export TF_NEED_GCP=0
-export TF_NEED_HDFS=0
 export TF_NEED_OPENCL_SYCL=0
 export TF_NEED_MKL=0
 export TF_NEED_VERBS=0
 export TF_NEED_MPI=0
-export TF_NEED_AWS=0
-export TF_NEED_KAFKA=0
+export TF_NEED_IGNITE=0
 export TF_NEED_GDR=0
 export TF_NEED_NGRAPH=0
 export TF_DOWNLOAD_CLANG=0
