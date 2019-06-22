89fe1e33b43fec1e884eaf23c4c1986b5398c06e
Reuben Morais <reuben.morais@gmail.com>
2019-06-22 10:17:44 -0300

    Update taskcluster-github-decision

--- tc-schedule.sh
+++ tc-schedule.sh
@@ -6,6 +6,6 @@ curdir=$(dirname "$0")
 
 pip3 install --quiet --user --upgrade pip
 
-curl -L --silent https://raw.githubusercontent.com/lissyx/taskcluster-github-decision/ef67832e6657f43e139a10f37eb326a7d9d96dad/requirements.txt | pip3 install --quiet --user --upgrade -r /dev/stdin
-curl -L --silent https://raw.githubusercontent.com/lissyx/taskcluster-github-decision/ef67832e6657f43e139a10f37eb326a7d9d96dad/tc-decision.py > ${curdir}/tc-decision.py
+curl -L --silent https://raw.githubusercontent.com/lissyx/taskcluster-github-decision/42212007ae8b416944fcf579cf884f11bafd00d3/requirements.txt | pip3 install --quiet --user --upgrade -r /dev/stdin
+curl -L --silent https://raw.githubusercontent.com/lissyx/taskcluster-github-decision/42212007ae8b416944fcf579cf884f11bafd00d3/tc-decision.py > ${curdir}/tc-decision.py
 python3 ${curdir}/tc-decision.py
